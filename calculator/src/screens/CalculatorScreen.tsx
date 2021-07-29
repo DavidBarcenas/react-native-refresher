7c66153213955ec249dc4394a554e765cf1960e8
David Barcenas <daveepro@outlook.com>
2021-07-29 17:14:22 -0500

    create functions for non-numeric buttons

--- calculator/src/screens/CalculatorScreen.tsx
+++ calculator/src/screens/CalculatorScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Text, View } from 'react-native';
 import { Button } from '../components/Button';
 import { styles } from '../theme/appTheme';
@@ -6,73 +6,120 @@ import { styles } from '../theme/appTheme';
 const darkGray = '#2d2d2d';
 const orange   = '#ff9427';
 
+enum Operators {add, substract, multiply, divide}
+
 export const CalculatorScreen = () => {
-    const [currentText, setCurrentText] = useState('0')
-    const [prevText, setPrevText] = useState('0')
+    const [currentNumber, setCurrentNumber] = useState('0')
+    const [prevNumber, setPrevNumber] = useState('0')
+    const operation = useRef<Operators>()
 
-    const reset = () => setCurrentText('0')
+    const reset = () => {
+        setCurrentNumber('0')
+        setPrevNumber('0')
+    }
 
-    const builNumber = (num: string) => {
+    const builNumber = (digit: string) => {
         //  do no accept dobule point
-        if(currentText.includes('.') && num === '.') return;
+        if(currentNumber.includes('.') && digit === '.') return;
 
-        if(currentText.startsWith('0') || currentText.startsWith('-0')) {
+        if(currentNumber.startsWith('0') || currentNumber.startsWith('-0')) {
             // decimal point
-            if(num === '.') {
-                setCurrentText(currentText + num)
-            } else if(num === '0' && currentText.includes('.')) {
+            if(digit === '.') {
+                setCurrentNumber(currentNumber + digit)
+            } else if(digit === '0' && currentNumber.includes('.')) {
                 // Exists zero or point
-                setCurrentText(currentText + num)
-            } else if(num !== '0' && !currentText.includes('.')) {
+                setCurrentNumber(currentNumber + digit)
+            } else if(digit !== '0' && !currentNumber.includes('.')) {
                 // validate if it is different from zero and has no point
-                setCurrentText(num)
-            } else if(num === '0' && !currentText.includes('.')) {
+                setCurrentNumber(digit)
+            } else if(digit === '0' && !currentNumber.includes('.')) {
                 //  avoid multiple leading zeros
-                setCurrentText(currentText)
+                setCurrentNumber(currentNumber)
             } else {
-                setCurrentText(currentText + num)
+                setCurrentNumber(currentNumber + digit)
             }
         } else {
-            setCurrentText(currentText + num)
+            setCurrentNumber(currentNumber + digit)
         }
 
     }
 
     const positiveNegative = () => {
-        if(currentText.includes('-')) {
-            setCurrentText(currentText.replace('-', ''))
+        if(currentNumber.includes('-')) {
+            setCurrentNumber(currentNumber.replace('-', ''))
         } else {
-            setCurrentText('-' + currentText)
+            setCurrentNumber('-' + currentNumber)
         }
+    }
+
+    const deleteDigit = () => {
+        if(currentNumber.length > 1) {
+            if(currentNumber.length <= 2 && currentNumber.includes('-')) {
+                setCurrentNumber('0')
+            } else {
+                setCurrentNumber(currentNumber.slice(0, -1))
+            }
+        } else {
+            setCurrentNumber('0')
+        }
+    }
+
+    const numberHistory = () => {
+        if(currentNumber.endsWith('.')) {
+            setPrevNumber(currentNumber.slice(0, -1))
+        } else {
+            setPrevNumber(currentNumber)
+        }
+        setCurrentNumber('0')
+    }
+
+    const handleDivision = () => {
+        numberHistory()
+        operation.current = Operators.divide
+    }
+    const handleMultiplication = () => {
+        numberHistory()
+        operation.current = Operators.multiply
+    }
+    const handleSubtraction = () => {
+        numberHistory()
+        operation.current = Operators.substract
+    }
+    const handleSum = () => {
+        numberHistory()
+        operation.current = Operators.add
     }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.secondaryText}>{prevText}</Text>
-            <Text style={styles.mainText} numberOfLines={1} adjustsFontSizeToFit>{currentText}</Text>
+            {
+                prevNumber !== '0' &&
+                <Text style={styles.secondaryText}>{prevNumber}</Text>
+            }
+            <Text style={styles.mainText} numberOfLines={1} adjustsFontSizeToFit>{currentNumber}</Text>
             <View style={styles.row}>
                 <Button onPress={reset} text='C' color={darkGray} />
                 <Button onPress={positiveNegative} text='+/-' color={darkGray} />
-                <Button onPress={reset} text='del' color={darkGray} />
-                <Button onPress={reset} text='/' background={orange} />
+                <Button onPress={deleteDigit} text='del' color={darkGray} />
+                <Button onPress={handleDivision} text='/' background={orange} />
             </View>
             <View style={styles.row}>
                 <Button onPress={builNumber} text='7' background={darkGray} />
                 <Button onPress={builNumber} text='8' background={darkGray} />
                 <Button onPress={builNumber} text='9' background={darkGray} />
-                <Button onPress={reset} text='X' background={orange} />
+                <Button onPress={handleMultiplication} text='X' background={orange} />
             </View>
             <View style={styles.row}>
                 <Button onPress={builNumber} text='4' background={darkGray} />
                 <Button onPress={builNumber} text='5' background={darkGray} />
                 <Button onPress={builNumber} text='6' background={darkGray} />
-                <Button onPress={reset} text='-' background={orange} />
+                <Button onPress={handleSubtraction} text='-' background={orange} />
             </View>
             <View style={styles.row}>
                 <Button onPress={builNumber} text='1' background={darkGray} />
                 <Button onPress={builNumber} text='2' background={darkGray} />
                 <Button onPress={builNumber} text='3' background={darkGray} />
-                <Button onPress={reset} text='+' background={orange} />
+                <Button onPress={handleSum} text='+' background={orange} />
             </View>
             <View style={styles.row}>
                 <Button onPress={builNumber} text='0' background={darkGray} extend />
