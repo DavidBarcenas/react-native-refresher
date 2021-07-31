67b831def911b9627b0e086403f62eaa78a73665
David Barcenas <daveepro@outlook.com>
2021-07-31 18:54:10 -0500

    send params to view

--- navigation/src/navigation/StackNavigator.tsx
+++ navigation/src/navigation/StackNavigator.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Page1Screen } from '../screens/Page1Screen';
 import { Page2Screen } from '../screens/Page2Screen';
 import { Page3Screen } from '../screens/Page3Screen';
+import { PersonScreen } from '../screens/PersonScreen';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ export const StackNavigation = () => {
       <Stack.Screen name="Page1Screen" options={{title: 'Page 1'}} component={Page1Screen} />
       <Stack.Screen name="Page2Screen" options={{title: 'Page 2'}} component={Page2Screen} />
       <Stack.Screen name="Page3Screen" options={{title: 'Page 3'}} component={Page3Screen} />
+      <Stack.Screen name="PersonScreen" options={{title: 'Person'}} component={PersonScreen} />
     </Stack.Navigator>
   );
 }
--- navigation/src/screens/PersonScreen.tsx
+++ navigation/src/screens/PersonScreen.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StackScreenProps } from '@react-navigation/stack';
 import { Button, Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> {}
 
-export const Page1Screen = ({navigation}: Props) => {
+export const PersonScreen = ({navigation, route}: Props) => {
+    useEffect(() => {
+        navigation.setOptions({
+            title: route?.params?.name
+        })
+    }, [])
+    
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Page1Screen</Text>
+            <Text style={styles.title}>PersonScreen</Text>
+            <Text style={styles.title}>{route?.params?.id} - {route?.params?.name}</Text>
             <Button title="Go to page 2" onPress={() => navigation.navigate('Page2Screen')} />
-            <Button title="Go to person" onPress={() => navigation.navigate('PersonScreen', {
-                id: 1,
-                name: 'David'
-            })} />
         </View>
     )
 }
