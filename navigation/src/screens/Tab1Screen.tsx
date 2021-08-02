0746a6976369732bfe095eaba63604e56b47981e
David Barcenas <daveepro@outlook.com>
2021-08-02 13:55:37 -0500

    configure icons

--- navigation/src/screens/Tab1Screen.tsx
+++ navigation/src/screens/Tab1Screen.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 export const Tab1Screen = () => {
     return (
         <View style={{...styles.tab, backgroundColor: '#2196f3'}}>
+            <Icon name="rocket" size={50} color="#fff" />
             <Text style={styles.tabContent}>Tab1 Screen</Text>
         </View>
     )
