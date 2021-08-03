d9712591f18face43f8317deb3ad80563e0a2f1d
David Barcenas <daveepro@outlook.com>
2021-08-02 20:44:18 -0500

    consume auth context

--- navigation/src/screens/SettingsScreen.tsx
+++ navigation/src/screens/SettingsScreen.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { AuthContext } from '../context/AuthContext';
 import { styles } from '../theme/appTheme';
 
 export const SettingsScreen = () => {
     const insets = useSafeAreaInsets()
+    const {authState} = useContext(AuthContext)
 
     return (
         <View style={{marginTop: insets.top}}>
             <Text style={styles.title}>Settings</Text>
+            <Text>{JSON.stringify(authState, null, 4)}</Text>
         </View>
     )
 }
