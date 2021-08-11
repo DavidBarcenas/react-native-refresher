118499ec30313c29ad5a7ada88a80ab686597c88
David Barcenas <daveepro@outlook.com>
2021-08-11 16:14:06 -0500

    configure stack navigator and icons

---
+++ components/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Icon from "react-native-vector-icons/Ionicons";
+
+export const HomeScreen = () => {
+    return (
+        <View>
+            <Icon name='home' />
+            <Text>Home</Text>
+        </View>
+    )
+}
