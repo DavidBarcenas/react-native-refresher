bfe16d9c2cf8e2d43fe4701579878f3c119fc7fd
David Barcenas <daveepro@outlook.com>
2021-08-11 19:33:01 -0500

    use easig bounce animation

--- components/src/screens/AnimationScreen.tsx
+++ components/src/screens/AnimationScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useRef } from 'react'
-import { View, StyleSheet, Animated, Button } from 'react-native';
+import { View, StyleSheet, Animated, Button, Easing } from 'react-native';
 
 export const AnimationScreen = () => {
     const opacity = useRef(new Animated.Value(0)).current
+    const top = useRef(new Animated.Value(-100)).current
 
     const fadeIn = (start = true) => {
         Animated.timing(opacity, {
@@ -10,6 +11,13 @@ export const AnimationScreen = () => {
             duration: 300,
             useNativeDriver: true
         }).start(() => console.log('end animation'))
+        
+        Animated.timing(top, {
+            toValue: start ? 0 : -150,
+            duration: 300,
+            useNativeDriver: true,
+            easing: Easing.bounce
+        }).start()
     }
 
     return (
@@ -17,7 +25,10 @@ export const AnimationScreen = () => {
             <Animated.View 
                 style={{
                     ...styles.box,
-                    opacity
+                    opacity,
+                    transform: [{
+                        translateY: top
+                    }]
                 }} 
             />
             <Button title="Fade In" onPress={() => fadeIn(true)} />
