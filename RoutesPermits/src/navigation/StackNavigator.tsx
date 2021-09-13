d70f25c56c51196596e8ed6bdc01b7681faf7ba2
David Barcenas <daveepro@outlook.com>
2021-09-13 12:48:42 -0500

    configure react-native-permissions

--- RoutesPermits/src/navigation/StackNavigator.tsx
+++ RoutesPermits/src/navigation/StackNavigator.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../Screens/HomeScreen';
+
+import { MapScreen } from '../screens/MapScreen';
+import { PermissionsScreen } from '../screens/PermissionsScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#fff'
+        }
+      }}
+    >
+      <Stack.Screen name="Map" component={MapScreen} />
+      <Stack.Screen name="Permissions" component={PermissionsScreen} />
     </Stack.Navigator>
   )
 }
--- RoutesPermits/src/screens/MapScreen.tsx
+++ RoutesPermits/src/screens/MapScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export const HomeScreen = () => {
+export const MapScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Map Screen</Text>
     </View>
   )
 }
