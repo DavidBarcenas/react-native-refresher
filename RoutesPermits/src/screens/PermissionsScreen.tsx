174ac65ee1f8286befe8a4962d1a2fce4887107b
David Barcenas <daveepro@outlook.com>
2021-09-13 15:12:54 -0500

    redirect to settings screen if permissions are blocked

--- RoutesPermits/src/screens/PermissionsScreen.tsx
+++ RoutesPermits/src/screens/PermissionsScreen.tsx
@@ -1,15 +1,25 @@
 import React, { useContext } from 'react'
-import { Button, Platform, StyleSheet, Text, View } from 'react-native'
-import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { openSettings } from 'react-native-permissions'
 import { PermissionContext } from '../context/PermissionContext'
 
 export const PermissionsScreen = () => {
   const { permissions, handleLocationPermission } = useContext(PermissionContext)
 
+  const handleRequest = () => {
+    handleLocationPermission('request')
+
+    if (permissions.locationStatus === 'denied' ||
+      permissions.locationStatus === 'blocked') {
+      openSettings()
+    }
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Permissions Screen</Text>
-      <Button title="Allowed" onPress={() => handleLocationPermission('request')} />
+      <Button title="Allowed" onPress={handleRequest} />
       <Text>{JSON.stringify(permissions, null, 4)}</Text>
     </View>
   )
