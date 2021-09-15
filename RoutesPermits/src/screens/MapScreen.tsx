8f4da793db18762add237f890de8465ace83d13e
David Barcenas <daveepro@outlook.com>
2021-09-15 12:51:02 -0500

    get user coordinates

--- RoutesPermits/src/screens/MapScreen.tsx
+++ RoutesPermits/src/screens/MapScreen.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 export const MapScreen = () => {
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => console.log(info),
+      err => console.log(err),
+      {
+        enableHighAccuracy: true
+      }
+    )
+  }, [])
+
   return (
     <View style={{ flex: 1 }}>
       <MapView
         style={{ flex: 1 }}
+        showsUserLocation
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
